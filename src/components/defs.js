388100387f3ae872010cc28556517809829ed5cd
Pavel Kalchenko <pavel.k.bz@gmail.com>
2018-12-17 17:03:49 +0300

    upd

--- src/components/defs.js
+++ src/components/defs.js
@@ -16,52 +16,8 @@ export default {
   startPos: [defX, defY],
   lineStartDots: [ [defX, defY], [xGrid(27), defY] ],
   viewbox: {
-<<<<<<< HEAD
     width: 1024,
     height: 658
-  },
-  table: {
-    width: xGrid(3),
-    widthB: xGrid(4),
-    height: xGrid(3),
-    radius: 8,
-    fill: '#edf1f2',
-    numberFont: {
-      size: 40,
-      fill: '#646666',
-      family: 'SFD, Helvetica, Arial, sans-serif',
-      weight: 200
-    },
-    guestsFont: {
-      size: '17px',
-      fill: '#8FACB3',
-      family: 'SFD, Helvetica, Arial, sans-serif',
-      weight: 200
-    }
-  },
-  textFont: {
-    size: 40,
-    family: 'SFD, sans-serif',
-    weight: 200
-  },
-  wallStroke: {
-    width: 22,
-    color: '#edf1f2',
-    linecap: 'round'
-  },
-  barStroke: {
-    width: 22,
-    color: '#ebd7ce',
-    linecap: 'round'
-  },
-  glassStroke: {
-    color: '#84d4fb',
-    width: 11,
-    linecap: 'round',
-    dasharray: '110 11'
-=======
-      width: 1024,
-      height: 658
   },
   table: {
     width: xGrid(3),
