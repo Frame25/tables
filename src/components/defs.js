94fe164daf134839659e30d65a342fceac94b21a
Pavel Kalchenko <pavel.k.bz@gmail.com>
2019-01-11 15:55:19 +0300

    ргн

--- src/components/defs.js
+++ src/components/defs.js
@@ -16,8 +16,8 @@ export default {
   startPos: [defX, defY],
   lineStartDots: [ [defX, defY], [xGrid(27), defY] ],
   viewbox: {
-    width: 1224,
-    height: 758
+    width: 1024,
+    height: 600
   },
   table: {
     width: xGrid(3),
